a92392e47bf832214066fc3e006ba317ad03ede4
Isaac Morais <isaacmoraisz2004@gmail.com>
2024-12-02 17:21:34 -0300

    Update PessoaDesaparecidaDAO.java

--- CadeVoce/src/main/java/com/example/CadeVoce/model/PessoaDesaparecidaDAO.java
+++ CadeVoce/src/main/java/com/example/CadeVoce/model/PessoaDesaparecidaDAO.java
@@ -1,6 +1,5 @@
 package com.example.CadeVoce.model;
 
-import com.example.CadeVoce.model.PessoaDesaparecida;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
